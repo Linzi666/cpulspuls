05691d748680d1427f543eed72b7ba37a31aa6f0
Lin2C <778129656@qq.com>
2022-11-01 13:31:24 +0800

    递归求 次方

---
+++ 2022.11/Power/Power/Power.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//递归实现 n 的 k 次方
+
+#include <stdio.h>
+int power(int n,int k)
+{
+	if (k != 0)
+	{
+		k--;
+		return n * power(n, k);
+	}
+	else
+		return 1;		
+}
+
+int main()
+{
+	int n = 0; //n
+	int k = 0; // k 次方
+	scanf("%d %d",&n,&k); // 分别输入 n 和 k 次方
+	int ret = power(n,k);
+	printf("%d 的 %d 次方 : %d",n,k,ret);
+
+	return 0;
+}
