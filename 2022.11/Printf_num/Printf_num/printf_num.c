67b5e7f527456ec2604a0266659d4921e9d0e6e9
Lin2C <778129656@qq.com>
2022-11-01 13:55:34 +0800

    递归方式 分别 打印 每位数

---
+++ 2022.11/Printf_num/Printf_num/printf_num.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//打印一个数的每一位 递归实现
+
+#include <stdio.h>
+
+void Printf(int num)
+{
+	//递归的限制条件，个位数 小于 9 后结束递推
+	if (num > 9) //递归限制条件
+		Printf(num/10); //递归 条件变化
+		
+	printf("%d ",num%10); //打印个位数 
+}
+
+int main()
+{
+	int num = 0; //要打印的数
+	scanf("%d",&num);
+	Printf(num); //函数无返回值，参数 num
+
+	return 0;
+}
