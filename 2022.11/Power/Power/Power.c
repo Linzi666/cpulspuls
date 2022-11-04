25b105b2eaae95de9136c528af5d605a2abde5e2
Lin2C <778129656@qq.com>
2022-11-04 13:17:22 +0800

    递归求 一个数的次方

--- 2022.11/Power/Power/Power.c
+++ 2022.11/Power/Power/Power.c
@@ -3,15 +3,18 @@
 //递归实现 n 的 k 次方
 
 #include <stdio.h>
-int power(int n,int k)
+double power(int n,int k)
 {
-	if (k != 0) //递归限制条件
-	{
-		k--; //递归条件变化
-		return n * power(n, k); 
-	}
+	 //第一种情况， k = 0 
+
+	if (k == 0)
+		return 1;
+	//第二种情况，k>0
+	else if (k > 0)
+		return n * power(n, k-1);
+	//第三种情况，k, < 0
 	else
-		return 1;		
+		return 1.0 * power(n, -k);
 }
 
 int main()
@@ -19,8 +22,8 @@ int main()
 	int n = 0; //n
 	int k = 0; // k 次方
 	scanf("%d %d",&n,&k); // 分别输入 n 和 k 次方
-	int ret = power(n,k);
-	printf("%d 的 %d 次方 : %d",n,k,ret);
+	double ret = power(n,k);
+	printf("%d 的 %d 次方 : %lf",n,k,ret);
 
 	return 0;
 }
