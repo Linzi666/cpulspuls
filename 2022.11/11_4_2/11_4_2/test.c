0838201a2da3a00c9626864ede23338850a8d64f
Lin2C <778129656@qq.com>
2022-11-10 22:31:05 +0800

    两个数的二进制位的不同个数

--- 2022.11/11_4_2/11_4_2/test.c
+++ 2022.11/11_4_2/11_4_2/test.c
@@ -5,6 +5,8 @@
 //编程实现：两个int（32位）整数m和n的二进制表达中，有多少个位(bit)不同？ 
 
 //写一个函数返回参数二进制中 1 的个数。
+
+//方法一，遍历32个位
 int get_binary(int n)
 {
 	//获取1的个数
@@ -16,6 +18,34 @@ int get_binary(int n)
 	return count;
 }
 
+//方法二
+int get_binary2(int n)
+{
+	//先转换成无符号整数，因为负数的存储方式不同
+	unsigned tep = n;
+	int count = 0;
+	while (tep)
+	{
+		if (tep % 2)
+			count++;
+		tep /= 2;
+	}
+
+	return count;
+}
+
+//神奇公式  n & (n-1) 
+int get_binary3(int n)
+{
+	int count = 0;
+	while (n)
+	{
+		n = n& (n - 1);
+			count++;
+	}
+	return count;
+}
+
 
 int main()
 {
@@ -24,16 +54,13 @@ int main()
 	int count = 0; //记录不同的个数
 	scanf("%d %d",&num1,&num2);
 	int num3 = num1 ^ num2; // 亦或，相同为 0 ，相异为 1
-	int i = 0;
-	//for (i = 0; i < 32; i++)
-	//{
-	//	//求出 num3 右移 & 1
-	//	if (num3 & (1<<i))
-	//		count++;
-	//}
 	count = get_binary(num3);
+	printf("方法一:%d\n",count);
 
-	printf("%d",count);
+	count = get_binary2(num3);
+	printf("方法二:%d\n", count);
 
+	count = get_binary3(num3);
+	printf("方法三:%d\n", count);
 	return 0;
 }
