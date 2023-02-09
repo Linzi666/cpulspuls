687fc05bb7549a9100ee588d7ba704f738d97c4e
@Lin2C <778129656@qq.com>
2023-02-09 08:14:59 +0800

    进程虚拟空间测试

---
+++ 2023.2/proaddrtest/test.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int g_val = 100;
+
+int main()
+{
+  int pid = fork(); //创建子进程
+  if(pid == 0)
+  {
+    //child
+    int count = 5;
+    while(count)
+    {
+      printf("i am child , g_val = %d, &g_val = %p\n",g_val,&g_val);
+
+      if(count == 3)
+      {
+        //修改数据
+        printf("******开始修改数据*******\n");
+        printf("i am child , g_val = %d, &g_val = %p\n",g_val,&g_val);
+        g_val = 200;
+        printf("******修改数据done*******\n");
+      }
+      count--;
+      sleep(1);
+    } 
+    
+  }
+  else if(pid > 0)
+  {
+    //parent
+    while(1)
+    {
+      printf("i am father , g_val = %d, &g_val = %p\n",g_val,&g_val);
+      sleep(1);
+    }
+  }
+  else 
+  {
+    //erro
+    perror("fork:");
+  }
+
+}
