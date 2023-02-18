c3c072f32209ead5bbd156784fb5a10205e0f83a
@Lin2C <778129656@qq.com>
2023-02-18 23:56:06 +0800

    动态库静态库演示

---
+++ 2023.2/libTest/lib1Test/creatlib/output/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"add.h"
+#include"sub.h"
+
+int main()
+{
+  int x = 10;
+  int y = 20;
+  int r1 = add(x,y);
+  int r2 = sub(x,y);
+  printf("%d + %d = %d",x,y,r1);
+  printf("%d - %d = %d",x,y,r2);
+
+  return 0;
+}
